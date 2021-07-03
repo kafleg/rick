7395584b5f70397e6a4825ac5ab0e9fd1e823021
Sandilya Kafle <kafleg@users.noreply.github.com>
2021-07-03 09:56:36 +0545

    Initial submission on Git

---
+++ functions.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Rick
+ * @subpackage Rick
+ * @since 1.0.0
+ */
+
+if ( ! function_exists( 'rick_setup' ) ) {
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	function rick_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Clove Blocks, use a find and replace
+		 * to change 'rick' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'rick' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1568, 9999 );
+
+		// Add support for Block Styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( array( 
+			'./inc/patterns/block-style.css'
+		) );
+
+		// Add support for responsive embedded content.
+		add_theme_support( 'responsive-embeds' );
+
+		// Add support for experimental link color control.
+		add_theme_support( 'experimental-link-color' );
+
+		// Add support for custom units.
+		add_theme_support( 'custom-units' );
+	}
+}
+add_action( 'after_setup_theme', 'rick_setup' );
+
+/**
+ * Show '(no title)' in frontend if post has no title to make it selectable
+ */
+add_filter(
+	'the_title',
+	function( $title ) {
+		if ( ! is_admin() && empty( $title ) ) {
+			$title = __( '(no title)', 'rick' );
+		}
+
+		return $title;
+	}
+);
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Rick ===
+
+Contributors: wpentire
+Tags: custom-background, custom-logo, custom-menu, featured-images, threaded-comments, translation-ready, blog
+Requires at least: 5.3
+Tested up to: 5.8
+Requires PHP: 7.0
+License: GNU General Public License v2.0 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Full-Site-Editing experimental theme.
+
+== Description ==
+
+Free FSE WordPress theme. 
+
+== Copyright ==
+
+Rick WordPress Theme, 2021 WPEntire
+Rick is distributed under the terms of the GNU GPL.
+
+== License ==
+* Q WordPress Theme - GPL License 
+* Armando WordPress Theme - GPL License
+* Clove WordPress Theme - GPL License 
+* Full Site Editing - https://fullsiteediting.com/terms-conditions/
+
+== Screenshot Image ==
+CCO Image by Pxhere
+Source: https://pxhere.com/en/photo/590395
+License: https://pxhere.com/en/license
